b387bcf89e883f08cb178664dc06c052352c064d
Vitaliy Zhyvyliak <veltjj@gmail.com>
2022-05-25 15:46:27 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="contacts">
+        <div className="container">
+          <h1 className="contacts__title">
+            Contacts
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
